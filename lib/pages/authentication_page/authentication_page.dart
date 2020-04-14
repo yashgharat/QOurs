ae7deda45bfd0ba04214cac01824ceb546ae1720
Yash Gharat <yashgharat12@gmail.com>
2020-04-13 21:57:59 -0400

    flip card animation

--- lib/pages/authentication_page/authentication_page.dart
+++ lib/pages/authentication_page/authentication_page.dart
@@ -1,3 +1,4 @@
+import 'package:flip_card/flip_card.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:q_ours/extensions/hex_color.dart';
@@ -11,95 +12,63 @@ class AuthenticationPage extends StatefulWidget {
 
 class _AuthenticationPageState extends State<AuthenticationPage> {
   String buttonText;
+  String leadIn, link;
+  bool isAccount;
 
-  action(String username, String password) {}
+  GlobalKey<FlipCardState> cardKey = GlobalKey<FlipCardState>();
 
-  switchType(bool isAccount) {}
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-          child: ListView(
-        children: <Widget>[
-          TitleText('Sign in/up'),
-          AnimatedContainer(duration: Duration(seconds: 1), child: SignUp())
-        ],
-      )),
-    );
-  }
-}
-
-class SignIn extends StatefulWidget {
-  String helpText = "Need an account? Sign up here.";
-  String buttonText = "Login";
-
-  @override
-  _SignInState createState() => _SignInState();
-}
-
-class _SignInState extends State<SignIn> {
-  login(String username, String password) {
-    print("do login");
+  action(String username, String password) {
+    if (isAccount) {
+      print('login');
+    } else {
+      print('register');
+    }
   }
 
-  callback() {
-    print('callback');
+  _AuthenticationPageState() {
+    isAccount = true;
+    buttonText = 'Login';
+    leadIn = 'Need an Account? ';
+    link = 'Sign up';
+  }
+
+  switchType() {
+    cardKey.currentState.toggleCard();
   }
 
   @override
   Widget build(BuildContext context) {
-    RichText text = RichText(
+    RichText loginText = RichText(
         text: TextSpan(children: <TextSpan>[
-      TextSpan(text: 'Need an account? '),
+      TextSpan(text: 'Need an Account? ', style: TextStyle(color: Colors.black)),
       TextSpan(
-          text: 'Sign up here.',
+          text: 'Sign up' ,
           style: TextStyle(color: HexColor.fromHex('EB3505')),
-          recognizer: TapGestureRecognizer()..onTap = callback)
+          recognizer: TapGestureRecognizer()..onTap = switchType)
     ]));
 
-    final loginField = LoginField(widget.buttonText, text, login);
-    return Container(
-      color: Colors.transparent,
-      child: Center(
-        child: loginField,
-      ),
-    );
-  }
-}
-
-class SignUp extends StatefulWidget {
-  String helpText = "Already have an account? Login here.";
-  String buttonText = "Register";
-  @override
-  _SignUpState createState() => _SignUpState();
-}
-
-class _SignUpState extends State<SignUp> {
-  signup(String username, String password) {
-    print("do signup");
-  }
-
-  callback() {
-    print('callback');
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    RichText text = RichText(
-        text: TextSpan(style:TextStyle(color: Colors.black), children: <TextSpan>[
-      TextSpan(text: 'Already have an account? '),
+    RichText registerText = RichText(
+        text: TextSpan(children: <TextSpan>[
+      TextSpan(text: 'Already have an account? ', style: TextStyle(color: Colors.black)),
       TextSpan(
-          text: 'Login here.',
+          text: 'Login',
           style: TextStyle(color: HexColor.fromHex('EB3505')),
-          recognizer: TapGestureRecognizer()..onTap = callback)
+          recognizer: TapGestureRecognizer()..onTap = switchType)
     ]));
 
-    final loginField = LoginField(widget.buttonText, text, signup);
     return Container(
-      child: Center(
-        child: loginField,
-      ),
-    );
+        child: ListView(
+      children: <Widget>[
+        TitleText('Sign in/up'),
+        FlipCard(
+          key: cardKey,
+          flipOnTouch: false,
+          direction: FlipDirection.HORIZONTAL,
+          speed: 1000,
+          front: Center(child: LoginField('Login', loginText, action)),
+          back: Center(child: LoginField('Register', registerText, action)),
+        )
+      ],
+    ));
   }
 }
