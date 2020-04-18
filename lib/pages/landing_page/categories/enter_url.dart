8ab2988b59b6ac43d3350523b79b53a7b645ac3b
Amarjeet <60272668+amarjeet-kaloty@users.noreply.github.com>
2020-04-17 22:36:59 -0400

    Http route for Submit button

--- lib/pages/landing_page/categories/enter_url.dart
+++ lib/pages/landing_page/categories/enter_url.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:q_ours/widgets/material_parts.dart';
 import 'package:q_ours/widgets/landing_page_divider.dart';
 import 'package:q_ours/widgets/styled_form_field.dart';
-
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
 
 class EnterURL extends StatelessWidget {
   final textController = TextEditingController();
@@ -10,6 +12,25 @@ class EnterURL extends StatelessWidget {
   Function() callback;
 
   EnterURL(this.callback);
+  
+  //Enter URL
+  String url = 'https://randomuser.me/api/';
+
+  Future<String> makeRequest() async {
+    var response = await http.get(Uri.encodeFull(url),
+    //Only accept JSON response
+    headers: {"Accept": "application/json"});
+
+    List data;
+    var extractData = jsonDecode(response.body);
+    //I am not sure how data is stored.
+    data = extractData["GET-INFO"];
+
+    //Printing data on the console for Testing
+    print(data);
+    
+    return "Success";
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,6 +40,8 @@ class EnterURL extends StatelessWidget {
         StyledFormField(650, 'Enter URL', textController),
         QOursSubmitLinkButton('Submit', this.callback)
       ],
+      // Implement onPressed() to makeRequest()
+        onPressed: makeRequest,
     );
   }
 }
